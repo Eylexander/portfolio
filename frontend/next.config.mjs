1ad501fc9e5506c7792bab714bd0276cb92eea68
Eylexander <eylexander88@gmail.com>
2025-05-23 17:57:23 +0200

    wip(animations): Newer style with react-spring

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
  
 const withNextIntl = createNextIntlPlugin();
 
@@ -26,6 +27,18 @@ const withMDX = createMDX({
 const nextConfig = {
   output: 'standalone',
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'static/css/[name].[contenthash:8].css',
+          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+        })
+      );
+    }
+    
+    return config;
+  },
 };
  
 export default withMDX(withNextIntl(nextConfig));
